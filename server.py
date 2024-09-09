d7047af404b130a717a03f36d80e4b53ff6be734
luzheminlulu <luzheminlulu@hotmail.com>
2024-09-09 22:47:16 +0800

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ httpd.socket = ssl.wrap_socket(httpd.socket,
                                 certfile='/mnt/disk1/unraid/ssl/cert.pem',  
                                 keyfile='/mnt/disk1/unraid/ssl/key.pem',  
                                 server_side=True,
-			        ssl_version=ssl.PROTOCOL_TLSv1_2)  
+                                ssl_version=ssl.PROTOCOL_TLSv1_2)  
 
 print("Serving on https://[::]:4443")  
 httpd.serve_forever()  
